04d39afc16b17e2fd7e9a887b7ec4c9422a5ab75
kiddos <josephyu831007@yahoo.com.tw>
2016-09-23 00:26:03 +0800

    add file name toggle declaration

--- src/kdebug/debug.hpp
+++ src/kdebug/debug.hpp
@@ -21,20 +21,18 @@ enum level {
     ERROR
 };
 
-void file_logging_on(const std::string &filename);
-void file_logging_off(const std::string &filename);
+void file_logging_on();
+void file_logging_off();
+void file_name_on();
+void file_name_off();
 
 template <typename Clock, typename Duration>
 class dbg {
 public:
-    typedef std::tuple<typename Clock::rep, level, std::string> log_t;
-    typedef typename std::list<log_t>::iterator iterator;
-
     dbg(std::string unit);
     ~dbg();
 
     template<typename T> dbg &operator<<(T t) {
-        _ss << t;
         switch (_level) {
             case null:
                 return *this;
@@ -53,32 +51,20 @@ public:
         return *this;
     }
     dbg &output(level l, const std::string &file_name, int line_number);
-    void list();
-
-    iterator begin();
-    iterator end();
-    log_t back();
 
 private:
     typename Clock::time_point _starttime;
-    typename Clock::time_point _lastlogtime;
-    typename Clock::rep _time;
 
     std::string get_current_date_string();
     std::string get_level_string(level l);
     std::string get_current_time_string(bool update);
     void set_fileoutput(const std::string filename);
     static std::string current_timestr();
-    void log();
-
 
     level _level;
     bool _flag_logged, _should_update, _file_output;
-    time_t _current_console_time, _current_file_time;
     std::string _unit;
-    std::stringstream _ss;
     std::ofstream _output_file;
-    std::list<log_t> _log;
 };
 
 // global variables
@@ -89,6 +75,7 @@ typedef dbg<std::chrono::system_clock,
 typedef dbg<std::chrono::system_clock,
             std::chrono::seconds> Log;
 
+extern time_t _current_console_time, _current_file_time;
 extern Debug _debug;
 extern Timer _timer;
 extern Log _log;
