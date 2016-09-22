1e1ac6a0bddff886a8340fd5ac6688788996a2f6
kiddos <josephyu831007@yahoo.com.tw>
2016-09-23 00:05:46 +0800

    output date when change and add filename and line number

--- src/kdebug/debug.cpp
+++ src/kdebug/debug.cpp
@@ -22,20 +22,10 @@ string strcolor(string &&s,string &&color){
 
 namespace kdebug{
 
-map<level, const string> _levelstring = {
-    {null,    "null    "},
-    {file,    "file    "},
-    {info,    strcolor("info    ","32")},
-    {warning, strcolor("warning ","33")},
-    {error,   strcolor("error   ","31")},
-    {INFO,    strcolor("info    ","32")},
-    {WARNING, strcolor("warning ","33")},
-    {ERROR,   strcolor("error   ","31")},
-};
-
 template <typename Clock, typename Duration>
 dbg<Clock, Duration>::dbg(std::string unit)
-    : _starttime(Clock::now()), _flag_logged(true), unit(unit) {}
+    : _starttime(Clock::now()), _flag_logged(true),
+      _current_console_time(0), _current_file_time(0), _unit(unit) {}
 
 template <typename Clock, typename Duration>
 dbg<Clock, Duration>::~dbg() {
@@ -43,27 +33,28 @@ dbg<Clock, Duration>::~dbg() {
 }
 
 template <typename Clock, typename Duration>
-dbg<Clock, Duration> &dbg<Clock, Duration>::set_level(level l) {
-    _level = l;
+dbg<Clock, Duration> &dbg<Clock, Duration>::output(level l,
+                                                   const string &file_name,
+                                                   int line_number) {
+    // 1. write the date time if changes
+    // 2. write the logging level
+    // 3. write timing
+    // 4. write filename and line number
 
     bool update_time = false;
-    update_time = (l == file || l == info || l == warning || l == error);
+    update_time = (l == info || l == warning || l == error);
 
     stringbuf buf;
-    ostream output(&buf);
-    output << "[" << _levelstring[_level]
-        << current_timestr()
-        << " | " << time(update_time) << unit << "]: ";
+    ostream out(&buf);
+    out << std::flush << get_current_date_string() << std::flush
+        << '[' << get_level_string(l) << " | "
+        << get_current_time_string(update_time) << " | "
+        << file_name.substr(file_name.find_last_of("/")+1)
+        << ":" << line_number << "] ";
 
     switch (_level) {
         case null:
             return *this;
-        case FILE:
-        case file:
-            if (_output_file.is_open()) {
-                _output_file << std::endl << buf.str();
-            }
-            break;
         case INFO:
         case info:
         case WARNING:
@@ -76,6 +67,51 @@ dbg<Clock, Duration> &dbg<Clock, Duration>::set_level(level l) {
             break;
     }
     return *this;
+}
+
+template <typename Clock, typename Duration>
+string dbg<Clock, Duration>::get_current_date_string() {
+    stringbuf buf;
+    ostream out(&buf);
+
+    auto now = Clock::now();
+    time_t current_time = Clock::to_time_t(now);
+    struct tm *time_info = std::localtime(&current_time);
+    char buffer[1024] = {'\0'};
+    std::strftime(buffer, 1024, "[%m/%d/%y %A]\n", time_info);
+
+    if (_file_output) {
+        if (current_time - _current_file_time > 86400) {
+            _current_file_time = current_time;
+            out << buffer;
+        }
+    } else {
+        if (current_time - _current_console_time > 86400) {
+            _current_console_time = current_time;
+            out << buffer;
+        }
+    }
+    return buf.str();
+}
+
+template <typename Clock, typename Duration>
+string dbg<Clock, Duration>::get_level_string(level l) {
+    _level = l;
+
+    map<level, const string> levelstring = {
+        {null,    "null    "},
+        {info,    strcolor("info    ","32")},
+        {warning, strcolor("warning ","33")},
+        {error,   strcolor("error   ","31")},
+        {INFO,    strcolor("info    ","32")},
+        {WARNING, strcolor("warning ","33")},
+        {ERROR,   strcolor("error   ","31")},
+    };
+
+    stringbuf buf;
+    ostream out(&buf);
+    out << levelstring[_level];
+    return buf.str();
 }
 
 template <typename Clock, typename Duration>
@@ -99,17 +135,16 @@ void dbg<Clock, Duration>::log() {
 }
 
 template <typename Clock, typename Duration>
-string dbg<Clock, Duration>::time(bool update) {
+string dbg<Clock, Duration>::get_current_time_string(bool update) {
     const long timepassed = std::chrono::duration_cast<Duration>
         (Clock::now()-_starttime).count();
 
     if (update) _starttime = Clock::now();
 
     stringstream ss;
-    ss << timepassed;
+    ss << timepassed << _unit;
     return ss.str();
 }
-
 
 template <typename Clock, typename Duration>
 string dbg<Clock, Duration>::current_timestr() {
@@ -126,11 +161,11 @@ void dbg<Clock, Duration>::list() {
     if(!_flag_logged)
         log();
 
-    for(auto i = _log.begin() ; i!= _log.end() ; ++i) {
-        std::cout << std::get<0>(*i)
-            << _levelstring[std::get<1>(*i)]
-            << " : "<< std::get<2>(*i) <<'\n';
-    }
+    // for(auto i = _log.begin() ; i!= _log.end() ; ++i) {
+    //     std::cout << std::get<0>(*i)
+    //         << _levelstring[std::get<1>(*i)]
+    //         << " : "<< std::get<2>(*i) <<'\n';
+    // }
 }
 
 template <typename Clock, typename Duration>
