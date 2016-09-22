c65dad7faf101fec5547f840d9ed4888775875a5
kiddos <josephyu831007@yahoo.com.tw>
2016-09-23 00:26:17 +0800

    remove old loggins

--- src/kdebug/debug.cpp
+++ src/kdebug/debug.cpp
@@ -1,4 +1,3 @@
-#include <streambuf>
 #include <ctime>
 #include <iomanip>
 
@@ -9,8 +8,6 @@ using std::cerr;
 using std::map;
 using std::ostream;
 using std::string;
-using std::streambuf;
-using std::stringbuf;
 using std::stringstream;
 
 const string s_esc(1,(char)27);
@@ -24,8 +21,7 @@ namespace kdebug{
 
 template <typename Clock, typename Duration>
 dbg<Clock, Duration>::dbg(std::string unit)
-    : _starttime(Clock::now()), _flag_logged(true),
-      _current_console_time(0), _current_file_time(0), _unit(unit) {}
+    : _starttime(Clock::now()), _flag_logged(true), _unit(unit) {}
 
 template <typename Clock, typename Duration>
 dbg<Clock, Duration>::~dbg() {
@@ -44,8 +40,7 @@ dbg<Clock, Duration> &dbg<Clock, Duration>::output(level l,
     bool update_time = false;
     update_time = (l == info || l == warning || l == error);
 
-    stringbuf buf;
-    ostream out(&buf);
+    stringstream out;
     out << std::flush << get_current_date_string() << std::flush
         << '[' << get_level_string(l) << " | "
         << get_current_time_string(update_time) << " | "
@@ -59,11 +54,11 @@ dbg<Clock, Duration> &dbg<Clock, Duration>::output(level l,
         case info:
         case WARNING:
         case warning:
-            cout << std::endl << buf.str();
+            cout << std::endl << out.str();
             break;
         case ERROR:
         case error:
-            cerr << std::endl << buf.str();
+            cerr << std::endl << out.str();
             break;
     }
     return *this;
@@ -71,8 +66,7 @@ dbg<Clock, Duration> &dbg<Clock, Duration>::output(level l,
 
 template <typename Clock, typename Duration>
 string dbg<Clock, Duration>::get_current_date_string() {
-    stringbuf buf;
-    ostream out(&buf);
+    stringstream out;
 
     auto now = Clock::now();
     time_t current_time = Clock::to_time_t(now);
@@ -91,7 +85,7 @@ string dbg<Clock, Duration>::get_current_date_string() {
             out << buffer;
         }
     }
-    return buf.str();
+    return out.str();
 }
 
 template <typename Clock, typename Duration>
@@ -108,10 +102,9 @@ string dbg<Clock, Duration>::get_level_string(level l) {
         {ERROR,   strcolor("error   ","31")},
     };
 
-    stringbuf buf;
-    ostream out(&buf);
-    out << levelstring[_level];
-    return buf.str();
+    stringstream ss;
+    ss << levelstring[_level];
+    return ss.str();
 }
 
 template <typename Clock, typename Duration>
@@ -123,27 +116,15 @@ void dbg<Clock, Duration>::set_fileoutput(const string filename) {
 }
 
 template <typename Clock, typename Duration>
-void dbg<Clock, Duration>::log() {
-    string s;
-    _ss >> s;
-
-    _log.push_back(std::make_tuple(_time, _level, s));
-    _ss.str(string());
-    _ss.clear();
-    s.clear();
-    _flag_logged = true;
-}
-
-template <typename Clock, typename Duration>
 string dbg<Clock, Duration>::get_current_time_string(bool update) {
     const long timepassed = std::chrono::duration_cast<Duration>
         (Clock::now()-_starttime).count();
 
     if (update) _starttime = Clock::now();
 
-    stringstream ss;
-    ss << timepassed << _unit;
-    return ss.str();
+    stringstream out;
+    out << timepassed << _unit;
+    return out.str();
 }
 
 template <typename Clock, typename Duration>
@@ -156,42 +137,6 @@ string dbg<Clock, Duration>::current_timestr() {
     return std::string(buffer);
 }
 
-template <typename Clock, typename Duration>
-void dbg<Clock, Duration>::list() {
-    if(!_flag_logged)
-        log();
-
-    // for(auto i = _log.begin() ; i!= _log.end() ; ++i) {
-    //     std::cout << std::get<0>(*i)
-    //         << _levelstring[std::get<1>(*i)]
-    //         << " : "<< std::get<2>(*i) <<'\n';
-    // }
-}
-
-template <typename Clock, typename Duration>
-typename dbg<Clock, Duration>::iterator dbg<Clock, Duration>::begin() {
-    return _log.begin();
-}
-
-template <typename Clock, typename Duration>
-typename dbg<Clock, Duration>::iterator dbg<Clock, Duration>::end() {
-    if(!_flag_logged)
-        log();
-    return _log.end();
-}
-
-template <typename Clock, typename Duration>
-typename dbg<Clock, Duration>::log_t dbg<Clock, Duration>::back() {
-    if(!_flag_logged)
-        log();
-    if (_log.size() > 0) {
-        return _log.back();
-    } else {
-        string empt = "empty";
-        return std::make_tuple(_time, null, empt);
-    }
-}
-
 template class dbg <std::chrono::high_resolution_clock,
                     std::chrono::microseconds>;
 template class dbg <std::chrono::system_clock,
@@ -199,30 +144,10 @@ template class dbg <std::chrono::system_clock,
 template class dbg <std::chrono::system_clock,
                     std::chrono::milliseconds>;
 
+time_t _current_console_time = 0;
+time_t _current_file_time = 0;
 Debug _debug(" ms");
 Timer _timer(" us");
 Log _log(" sec");
 
 }
-/*
-std::ostream& operator<<(std::ostream& os, kdebug::Debug::log_t& l) {
-    os<< std::get<0>(l)<< ' '
-      << kdebug::levelstring[std::get<1>(l)]<<" : "
-      << std::get<2>(l);
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, kdebug::Timer::log_t& l) {
-    os<< std::get<0>(l)<< ' '
-      << kdebug::levelstring[std::get<1>(l)]<<" : "
-      << std::get<2>(l);
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, kdebug::Log::log_t& l) {
-    os<< std::get<0>(l)<< ' '
-      << kdebug::levelstring[std::get<1>(l)]<<" : "
-      << std::get<2>(l);
-    return os;
-}
-*/
