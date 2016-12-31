ed3676dcc1eeca10279e08c7ec9c00236ab19a18
kiddos <josephyu831007@yahoo.com.tw>
2017-01-01 07:18:03 +0800

    use cpp implemenation

--- src/kdebug/debug_c.h
+++ src/kdebug/debug_c.h
@@ -1,12 +1,23 @@
-#pragma once
+#ifndef DEBUG_C_H
+#define DEBUG_C_H
 
-#include <stdio.h>
-#include <errno.h>
-
-#ifdef DEBUG_MESSAGE
-#define LOG(msg, ...) fprintf(stdout, "LOG: "msg"\n", ##__VA_ARGS__)
-#define ERROR(msg) fprintf(stderr, "ERROR: %s, code: %d\n", msg, errno)
-#else
-#define LOG(msg, ...)
-#define ERROR(msg)
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+enum KDebugSeverity { KDEBUG_INFO, KDEBUG_WARNING, KDEBUG_ERROR };
+
+void kdebug_log(enum KDebugSeverity sev, const char* filename, int lin_num,
+                const char* const message);
+void kdebug_timer(enum KDebugSeverity sev, const char* filename, int lin_num,
+                  const char* const message);
+void kdebug_date(enum KDebugSeverity sev, const char* filename, int lin_num,
+                 const char* const message);
+void kdebug_kdebug(enum KDebugSeverity sev, const char* filename, int lin_num,
+                   const char* const message);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* end of include guard: DEBUG_C_H */
